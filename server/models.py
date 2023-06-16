d4512ffdf11207bdb0524ccb2c8eacc50e57d079
Historia Montague <historia.montague@outlook.com>
2023-06-16 09:54:18 -0600

    Finished Lab

--- server/models.py
+++ server/models.py
@@ -11,7 +11,19 @@ class Author(db.Model):
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError
+        return phone_number
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +39,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-
+    @validates('title')
+    def validate_title(self, key, title):
+        if any(clickbait not in title for clickbait in ("Won't believe", "Secret", "Top", "Guess")):
+            raise ValueError
+        return title
+    
+    @validates("content")
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError
+        return content
+    
+    @validates("summary")
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError
+        return summary
+    
+    @validates("category")
+    def validate_category(self, key, category):
+        if category not in ("Fiction", "Non-Fiction"):
+            raise ValueError
+        return category
+    
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
